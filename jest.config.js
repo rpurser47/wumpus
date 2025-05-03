41415b4be4dccb1e74b376fdeb70246f8e4e86ff
Rob Purser <rob@purser.org>
2025-05-03 10:36:10 -0400

    Update npm audit flag and reduce Jest verbosity

--- jest.config.js
+++ jest.config.js
@@ -8,49 +8,42 @@ const config = {
   clearMocks: true,
 
   // Use v8 for code coverage
-  coverageProvider: "v8",
-  
+  coverageProvider: 'v8',
+
   // Enable collecting coverage information
   collectCoverage: false,
-  
+
   // Configure coverage collection
   collectCoverageFrom: [
-    "src/**/*.{ts,tsx}",
-    "!src/**/*.d.ts",
-    "!src/**/__tests__/**",
-    "!src/**/__mocks__/**"
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/__tests__/**',
+    '!src/**/__mocks__/**',
   ],
-  
+
   // Coverage directory
-  coverageDirectory: "coverage",
-  
+  coverageDirectory: 'coverage',
+
   // Coverage thresholds
   coverageThreshold: {
     global: {
       statements: 70,
       branches: 70,
       functions: 70,
-      lines: 70
-    }
+      lines: 70,
+    },
   },
 
   // Using default Node environment for now
   // testEnvironment: "jsdom",
 
   // File extensions to use
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 
   // Handle asset imports in tests
   moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "<rootDir>/src/__mocks__/styleMock.js",
-    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/src/__mocks__/fileMock.js"
+    '\\.(css|less|scss|sass)$': '<rootDir>/src/__mocks__/styleMock.js',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/src/__mocks__/fileMock.js',
   },
 
   // We'll add setup files later when needed
@@ -58,24 +51,17 @@ const config = {
 
   // Use ts-jest for TypeScript files
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
 
   // Test matching patterns
-  testMatch: [
-    "**/__tests__/**/*.[jt]s?(x)",
-    "**/?(*.)+(spec|test).[jt]s?(x)"
-  ],
-  
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+
   // Explicitly exclude mocks from being treated as tests
-  testPathIgnorePatterns: [
-    "/node_modules/",
-    "/__mocks__/",
-    "/__tests__/mocks/"
-  ],
+  testPathIgnorePatterns: ['/node_modules/', '/__mocks__/', '/__tests__/mocks/'],
 
   // Be verbose in test output
-  verbose: true
+  verbose: false,
 };
 
 module.exports = config;
